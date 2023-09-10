fbada9d0103b79599ddeabf24fe69deb5781b581
Shobhit Jaguri <110186829+Shobhitja@users.noreply.github.com>
2023-09-10 21:27:59 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./Src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
